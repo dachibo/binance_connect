eac5431b1b76c7f1e5e6687df2c930844bf3c05e
Даниил Павленко <d.pavlenko@mixvel.com>
2023-02-19 14:03:34 +0500

    pinbar.py

--- patterns/pinbar.py
+++ patterns/pinbar.py
@@ -1,11 +1,12 @@
-def pinbar(bars: list):
+def pinbar(current_bar, prev_bar, total_average: float) -> bool:
     """
-    # Bullish pinbar
-  df.loc[idx,'Bullish pinbar'] = realbody <= candle_range/3 and  min(current['Open'], current['Close']) > (current['High'] + current['Low'])/2 and current['Low'] < prev['Low']
 
-  # Bearish pinbar
-  df.loc[idx,'Bearish pinbar'] = realbody <= candle_range/3 and max(current['Open'] , current['Close']) < (current['High'] + current['Low'])/2 and current[
-    :param bars:
+    :param total_average:
+    :param prev_bar:
+    :param current_bar:
     :return:
     """
-    pass
+    return (current_bar.Average <= total_average / 3 and min(current_bar.Open, current_bar.Close) >
+            (current_bar.High + current_bar.Low) / 2 and current_bar.Low < prev_bar.Low) or \
+        (current_bar.Average <= total_average / 3 and min(current_bar.Open, current_bar.Close) <
+         (current_bar.High + current_bar.Low) / 2 and current_bar.Low < prev_bar.Low)
